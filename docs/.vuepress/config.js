642b2684c3346a783950843a2702b3eb82aff677
ImpressGroup <ib-wd@impress.co.jp>
2018-12-05 09:27:57 +0800

    update git account

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,13 @@
 module.exports = {
   title: 'Netlify CMS + VuePress',
   description: 'Netlify + VuePress',
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
   themeConfig: {
     docsDir: 'docs',
-    repo: 'andreliem/vuepress-netlify-cms',
+    repo: 'ImpressGroup/vuepress',
     sidebar: [
       '/',
       '/welcome',
